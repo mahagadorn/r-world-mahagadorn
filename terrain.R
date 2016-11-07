91b85c895ed979e1f9bab8893cec26e5014bf6a1
mahagadorn <mahagadorn@aggiemail.usu.edu>
2016-11-06 19:08:16 -0700

    terrain.R

--- terrain.R
+++ terrain.R
@@ -3,25 +3,74 @@
 ##Terrain
 ##November 2, 2016
 
-#first thing we need to do is to make the square.matrix function
 
-diamond.matrix <- function(x){
-  matrix <- matrix(NA, nrow = ((2*x)+1), ncol = ((2*x)+1))
-  matrix[1,1] <- rnorm(1)
-  matrix[nrow(matrix), 1] <- rnorm(1)
-  matrix[1, ncol(matrix)] <- rnorm(1)
-  matrix[nrow(matrix), nrow(matrix)] <- rnorm(1)
+#first thing that we need to do is to make a general matrix.
+#matrix is called the g.mat, so we are saving these in specific places.
+general.matrix <- function(n){
+  mat.size <- ((2*n)+1)
+  g.mat <- matrix(NA, nrow = mat.size, ncol = mat.size)
+  #here we are adding values to the corners
+  g.mat[1,1] <- rnorm(1, 500, 500)
+  g.mat[nrow(g.mat), 1] <- rnorm(1, 500, 500)
+  g.mat[1, ncol(g.mat)] <- rnorm(1, 500, 500)
+  g.mat[nrow(g.mat), nrow(g.mat)] <- rnorm(1, 500, 500)
+  return(g.mat)
+}
+
+g.mat <- general.matrix(4) 
+
+
+#diamond.step function
+diamond.step <- function(g.mat){
+  corner.vec <- c(g.mat[1,1], g.mat[1, ncol(g.mat)],  g.mat[1, ncol(g.mat)], g.mat[nrow(g.mat), nrow(g.mat)])    #give me the values of the corners
+  g.mat[ceiling(.5*(nrow(g.mat))), ceiling(.5*(ncol(g.mat)))] <- mean(corner.vec)     #taking the mean of the corner and placing it in the center of the matrix
+  #now we need to add in to make the mini boxes
+  #Start with the top left
+  g.mat[ceiling(.5*nrow(g.mat)), 1] <- rnorm(1, 250, 100) #bottom left corner of top left mini-square
+  g.mat[1, ceiling(.5*ncol(g.mat))] <- rnorm(1, 250, 100) #top right corner of the top left mini-square
+  tl.mid <- c(g.mat[1,1], g.mat[1, ceiling(.5*ncol(g.mat))], g.mat[ceiling(.5*nrow(g.mat)), 1], g.mat[ceiling(.5*(nrow(g.mat))), ceiling(.5*(ncol(g.mat)))])
+  #fill in the middle of the top corner
+  g.mat[ceiling((1/4)*nrow(g.mat)), ceiling((1/4)*ncol(g.mat))] <- mean(tl.mid)
+  #moving on to bottom left box
+  #all points but the bottom right corner of the bottom left box are already filled. Going to generate that here
+  g.mat[nrow(g.mat), ceiling(.5*ncol(g.mat))] <- rnorm(1, 250, 100)  #bottom right corner of bottom left box
+  bl.mid <- c(g.mat[1, ceiling(.5*ncol(g.mat))], g.mat[ceiling(.5*(nrow(g.mat))), ceiling(.5*(ncol(g.mat)))], g.mat[nrow(g.mat), ncol(g.mat)], g.mat[nrow(g.mat), ceiling(.5*ncol(g.mat))])
+  g.mat[ceiling((3/4)*nrow(g.mat)), ceiling((1/4)*ncol(g.mat))] <- mean(bl.mid)
+  #working on the bottom right box
+  g.mat[ceiling(.5*nrow(g.mat)), ncol(g.mat)] <- rnorm(1, 250, 100)
+  br.mid <- c(g.mat[ceiling(.5*nrow(g.mat)), ncol(g.mat)], g.mat[ceiling(.5*(nrow(g.mat))), ceiling(.5*(ncol(g.mat)))], g.mat[nrow(g.mat), ceiling(.5*(ncol(g.mat)))], g.mat[nrow(g.mat), ncol(g.mat)])
+  g.mat[ceiling((3/4)*nrow(g.mat)), ceiling((3/4)*ncol(g.mat))] <- mean(br.mid)
+  #now just need to add the middle of the top right box
+  tr.mid <- c(g.mat[ceiling(.5*nrow(g.mat)), ncol(g.mat)], g.mat[ceiling(.5*(nrow(g.mat))), ceiling(.5*(ncol(g.mat)))], g.mat[nrow(g.mat), ceiling(.5*(ncol(g.mat)))], g.mat[1, ncol(g.mat)])
+  g.mat[ceiling((1/4)*nrow(g.mat)), ceiling((3/4)*ncol(g.mat))] <- mean(tr.mid)
+  return(g.mat)
+} 
+
+g.mat <- diamond.step(g.mat)
+
+
+
+
+#first thing we need to do is to make the diamond.matrix function
+
+diamond.step <- function(g.mat){
+  matrix <- g.mat
   corner.vec <- c(matrix[1,1],  matrix[1, ncol(matrix)],  matrix[1, ncol(matrix)], matrix[nrow(matrix), nrow(matrix)])
   matrix[ceiling(.5*(nrow(matrix))), ceiling(.5*(ncol(matrix)))] <- mean(corner.vec)
   return(matrix)
 } 
 
-a <- diamond.matrix(x=5)
+diamond.matrix(g.mat)
 
-square.matrix <- function(x){
+
+
+
+
+square.step <- function(x){
+  matrix <- matrix(NA, nrow = ((2*x)+1), ncol = ((2*x)+1))
   matrix[ceiling(.5*nrow(matrix)),1] <- mean(c(matrix[1,1], matrix[nrow(matrix),1],  matrix[ceiling(.5*(nrow(matrix))), ceiling(.5*(ncol(matrix)))]))
   matrix[ceiling(.5*nrow(matrix)), ceiling(.5*ncol(matrix))] <- mean(c(matrix[1, ceiling(.5*ncol(matrix))],  matrix[nrow(matrix), nrow(matrix)],  matrix[ceiling(.5*(nrow(matrix))), ceiling(.5*(ncol(matrix)))]))
   return(matrix)
 }
 
-square.matrix(a)
+square.matrix(4)
