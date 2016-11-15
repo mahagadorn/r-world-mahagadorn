245027feb9490f2b538d30ebb8a5f2de96a6c661
mahagadorn <mahagadorn@aggiemail.usu.edu>
2016-11-15 10:42:21 -0700

    terrain.R

--- terrain.R
+++ terrain.R
@@ -15,17 +15,15 @@
         #This is the only function that is exported!!!
 
 
-#matrix size
-# mat.size <- ((2^n)+1); will yield a matrix that will always have a center point/midpoints that can be calculated
-general.matrix <- function(n, mean){
+general.matrix <- function(n, x.bar){
   mat.size <- ((2^n)+1)
   g.mat <- matrix(NA, nrow = mat.size, ncol = mat.size)
   # here we are adding values to the corners
   # rnorm() adds a large amount of noise/variation to numeric outputs
-  g.mat[1,1] <- rnorm(1, mean, (mean+mean))
-  g.mat[nrow(g.mat), 1] <- rnorm(1, mean, (mean+mean))
-  g.mat[1, ncol(g.mat)] <- rnorm(1, mean, (mean+mean))
-  g.mat[nrow(g.mat), nrow(g.mat)] <- rnorm(1, mean, (mean+mean))
+  g.mat[1,1] <- rnorm(1, x.bar, (x.bar+x.bar))
+  g.mat[nrow(g.mat), 1] <- rnorm(1, x.bar, (x.bar+x.bar))
+  g.mat[1, ncol(g.mat)] <- rnorm(1, x.bar, (x.bar+x.bar))
+  g.mat[nrow(g.mat), nrow(g.mat)] <- rnorm(1, x.bar, (x.bar+x.bar))
   return(g.mat)
 }
 
@@ -43,7 +41,7 @@ diamond.step <- function(g.mat){
 # Square step matrix
 # This calculates the square step aspect of the terrain matrix
 # first we create vecotrs which concatenate the values that need to be input for calculating the means
-square.step <- function(g.mat, mean){
+square.step <- function(g.mat, x.bar){
   #need to make the left middle point
   corner.vec <- c(g.mat[1,1], g.mat[1, ncol(g.mat)],  g.mat[1, ncol(g.mat)], g.mat[nrow(g.mat), nrow(g.mat)])    #give me the values of the corners
   g.mat[ceiling(.5*(nrow(g.mat))), ceiling(.5*(ncol(g.mat)))] <- mean(corner.vec)
@@ -52,10 +50,10 @@ square.step <- function(g.mat, mean){
   right.mid <- c(g.mat[ceiling(.5*(nrow(g.mat))), ceiling(.5*(ncol(g.mat)))], g.mat[1, ncol(g.mat)], g.mat[nrow(g.mat), ncol(g.mat)])
   b.mid <- c(g.mat[ceiling(.5*(nrow(g.mat))), ceiling(.5*(ncol(g.mat)))], g.mat[1,1], g.mat[nrow(g.mat), ncol(g.mat)])
   #assigning means to spot in matrix; the rnorm() adds a decreased level of noise in this step
-  g.mat[ceiling(.5*(nrow(g.mat))), 1] <- mean(left.mid) + rnorm(1, mean, sd=(mean/2))
-  g.mat[1, ceiling(.5*(ncol(g.mat)))] <- mean(top.mid) + rnorm(1, mean, sd=(mean/2))
-  g.mat[ceiling(.5*(nrow(g.mat))), nrow(g.mat)] <- mean(right.mid) + rnorm(1, mean, sd=(mean/2))
-  g.mat[nrow(g.mat), ceiling(.5*(ncol(g.mat)))] <- mean(b.mid) + rnorm(1, mean, sd=(mean/2))
+  g.mat[ceiling(.5*(nrow(g.mat))), 1] <- mean(left.mid) + rnorm(1, x.bar, sd=(x.bar/2))
+  g.mat[1, ceiling(.5*(ncol(g.mat)))] <- mean(top.mid) + rnorm(1, x.bar, sd=(x.bar/2))
+  g.mat[ceiling(.5*(nrow(g.mat))), nrow(g.mat)] <- mean(right.mid) + rnorm(1, x.bar, sd=(x.bar/2))
+  g.mat[nrow(g.mat), ceiling(.5*(ncol(g.mat)))] <- mean(b.mid) + rnorm(1, x.bar, sd=(x.bar/2))
   return(g.mat)
 }
 
@@ -64,14 +62,14 @@ square.step <- function(g.mat, mean){
 
 #Writing a diamond step function that does both the functions above and fills in all values of our matrix
 #This is flexible enough to do large and small n values
-diamond.square.step<- function(g.mat, mean, n){
-  mean <- mean
+diamond.square.step<- function(g.mat, x.bar, n){
+  x.bar <- x.bar
   n <- n
   for(i in 2^(n:1)){
     for(j in seq(1, ncol(g.mat)-1, by=i)){
       for(k in seq(1, nrow(g.mat)-1, by=i)){
         g.mat[k:(k+i),j:(j+i)] <- diamond.step(g.mat[k:(k+i),j:(j+i)])
-        g.mat[k:(k+i),j:(j+i)] <- square.step(g.mat[k:(k+i),j:(j+i)], mean)
+        g.mat[k:(k+i),j:(j+i)] <- square.step(g.mat[k:(k+i),j:(j+i)], x.bar)
       }
     }
   }
@@ -83,7 +81,7 @@ diamond.square.step<- function(g.mat, mean, n){
 #'
 #' A light wrapper function around \code{diamond.square.step} and the addition of lakes to the terrain matrix
 #' @param n Size of the grid will be a (2^n)+1 (by default: n=5; a 33 by 33 grid)
-#' @param mean Numeric vector (Default: 500) that will be input as the mean into the rnorm() function.
+#' @param x.bar Numeric vector (Default: 500) that will be input as the mean into the rnorm() function.
 #'    Standard deviations used in the rnorm() function are generated from the mean value.
 #'    The first standard deviation value (calculated as mean * mean) will be used in \code{general.matrix} to generate  the random corner values from a normal distribution.
 #'    The second standard deviation value (calculated as mean/2) will be used in \code{square.step} for generating noise at the finer spatial scales.
@@ -96,12 +94,12 @@ diamond.square.step<- function(g.mat, mean, n){
 #' @export
 
 
-make.terrain <- function(n=5, mean=500, lake.na=TRUE){
+make.terrain <- function(n=5, x.bar=500, lake.na=TRUE){
   n <- n
-  mean <- mean
+  x.bar <- x.bar
   mat.size <- ((2^n)+1)
-  g.mat <- general.matrix(n, mean)
-  terrain <- diamond.square.step(g.mat, mean, n)
+  g.mat <- general.matrix(n, x.bar)
+  terrain <- diamond.square.step(g.mat, x.bar, n)
   #now we want to make the option to make anything less than zero NA or (H20)
   if(lake.na==TRUE){
     terrain[terrain < 0] <- NA
